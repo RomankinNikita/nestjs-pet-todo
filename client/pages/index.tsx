8d313a9f55d4c8d93c9992cf7172102ab8ef7e28
RomankinNikita <romankin94@mail.ru>
2022-05-18 11:32:25 +0300

    next client added

---
+++ client/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default Home;
